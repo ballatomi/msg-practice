d975185c4306c45c74a6c81b714f2b57d231c3b9
Tamas Balla <Tamas.Balla@msg.group>
2021-07-21 09:15:44 +0300

    JavaEE Hello world example

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tutorial JavaEE with wildfly
+
+## 1. Generate project
+
+```
+$ mvn -DarchetypeGroupId=org.codehaus.mojo.archetypes -DarchetypeArtifactId=webapp-javaee7 -DgroupId=com.msg.practice 
+-DartifactId=helloworld-wildfly -Dversion=1.1 -Dpackage=com.msg.practice -Darchetype.interactive=false --batch-mode --update-snapshots archetype:generate
+```
+
+## 2. Useful Links
+```
+$ mvn install wildfly:deploy
+```
+
+## 3. Useful Links
+https://www.tutorialspoint.com/ejb/index.htm
---
+++ src/main/java/com/msg/practice/rest/HelloService.java
@@ -0,0 +1,36 @@
+package com.msg.practice.rest;
+
+import com.msg.practice.dto.TestDto;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * @author ballat
+ */
+@Path("/hello")
+public class HelloService {
+
+    @GET
+    @Path("/text")
+    public String getHello() {
+        return "hello world!";
+    }
+
+    @GET
+    @Path("/json")
+    @Produces(MediaType.APPLICATION_JSON)
+    public TestDto getPropertyJSON() {
+        TestDto p = new TestDto("key example", "value example");
+        return p;
+    }
+
+    @GET
+    @Path("/xml")
+    @Produces(MediaType.APPLICATION_XML)
+    public TestDto getPropertyXML() {
+        TestDto p = new TestDto("key", "value");
+        return p;
+    }
+}
